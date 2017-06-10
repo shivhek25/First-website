aac6adcf6e648ae29628325bf19034e3d0bee1e4
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-06-10 12:07:39 +0530

    Learning Javascript.

---
+++ Web1.js
@@ -0,0 +1,13 @@
+console.log("Adding Javascript to my first website");
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3);
+console.log(Math.random*100);
+console.log(Math.floor(Math.random()*100));
+//Learning basic operators
+/*this is me
+  hey there*/
+var nyName="Shivam";
+console.log("My name is"+myName);
