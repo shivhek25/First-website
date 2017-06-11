b7309488b0e8fb3dfa472b37dce9a1205839268c
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-06-11 13:16:30 +0530

    Added alert

--- Web1.js
+++ Web1.js
@@ -1,18 +1,1 @@
-console.log("Adding Javascript to my first website");
-console.log(2+3);
-console.log(2-3);
-console.log(2*3);
-console.log(2/3);
-console.log(2%3);
-console.log(Math.random*100);
-console.log(Math.floor(Math.random()*100));
-//Learning basic operators
-/*this is me
-  hey there*/
-var nyName="Shivam";
-console.log("My name is"+myName);
-var details= ['Shivam','Singhal'];
-console.log(details);
-details.push('Hey','there');
-console.log(details.length);
-console.log(details);
+alert('Welcome to my page!');
