69cd6f0297b0006a83cb8a1937b0cc98b41e799e
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-06-11 18:29:15 +0530

    Completed JS and Jquery

--- Web1.js
+++ Web1.js
@@ -1,9 +1,20 @@
-alert('Welcome to my page!');
+/*alert('Welcome to my page!');
 var name=document.getElementsByClassName('Main Name');
-alert(name);
+alert(name);*/
 main()
 {
+  var $name= $('.main');
+  alert($name);
   $('.main').hide();
-  $('.main').fadeIn(5000);
+  $('.main').fadeIn(1000);
+  $('.values').hide();
+  $('.headings').on('click',function{
+    //$('.values').show();
+    //$(this).next().toggle();
+    $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Seen');
+  });
+
 }
 $(document).ready(main);
