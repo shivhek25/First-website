461f1ea9a67d7a30b78d188476a2835088cb7f4f
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-06-11 14:24:10 +0530

    made changes

--- Web1.js
+++ Web1.js
@@ -1,1 +1,9 @@
-alert('Welcome to my page!');
+/*alert('Welcome to my page!');
+var name=document.getElementsByClassName('Main Name');
+alert(name);*/
+main()
+{
+  $('.main').hide();
+  $('.main').fadeIn(5000);
+}
+$(document).ready(main);
